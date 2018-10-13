b0a2a02f570e227eef7fbec057b238acb114a08e
Promlert Lovichit <promlert@gmail.com>
2018-10-13 12:39:56 +0700

    Initial commit

---
+++ app/src/main/java/th/ac/dusit/dbizcom/travelsamutprakan/SplashActivity.java
@@ -0,0 +1,28 @@
+package th.ac.dusit.dbizcom.travelsamutprakan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        //ใส่โค้ดที่ต้องการใหเทำงานหลังจากครบเวลาที่กำหนด
+                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }, 2000
+        );
+    }
+}
